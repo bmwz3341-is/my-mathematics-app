cc55027f22f01a7f287d16d24a6ebc4e282aff7d
Sharon <bmwz3341@gmail.com>
2026-07-05 19:35:49 +0300

    start page bulding

---
+++ src/app/start/page.tsx
@@ -0,0 +1,9 @@
+export default function StartPage() {
+  return (
+    <div className="min-h-screen bg-white px-6 py-8 sm:px-10">
+      <h1 className="text-right text-2xl font-extrabold text-blue-600 sm:text-3xl">
+        מתמטיקה, המרות ופתרון משוואות
+      </h1>
+    </div>
+  );
+}
